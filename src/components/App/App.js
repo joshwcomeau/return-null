68f92ace620ffcb51a9602d65ce28f6b7c4ec551
Joshua Comeau <joshwcomeau@gmail.com>
2017-04-03 09:26:02 -0400

    Small tweaks to components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.styles';
 
 class App extends Component {
   render() {
+    const { children } = this.props;
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
 import App from './App';
-import './index.css';
+
+
+const TempPathComponent = ({ location: { pathname } }) => <div>{pathname}</div>;
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App>
+      <Route path="/1" component={Title} />
+      <Route path="/2" component={TempPathComponent} />
+      <Route path="/3" component={TempPathComponent} />
+      <Route path="/4" component={TempPathComponent} />
+    </App>
+  </Router>,
   document.getElementById('root')
 );
