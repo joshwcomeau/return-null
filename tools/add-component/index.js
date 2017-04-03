bd0f6abd7448adf730f8288cc3485c2bb06fd793
Joshua Comeau <joshwcomeau@gmail.com>
2017-04-03 09:16:59 -0400

    Bring in add-component script

---
+++ tools/add-component/index.js
@@ -0,0 +1,111 @@
+/* eslint-disable no-console, no-use-before-define */
+const path = require('path');
+const fs = require('fs');
+const changeCase = require('change-case');
+
+
+function run(ComponentName) {
+  if (!ComponentName) {
+    throw new Error(`
+      Please supply a component name!
+      'npm run add-component -- YourComponentName'
+    `);
+  } else if (!changeCase.isUpperCase(ComponentName[0])) {
+    throw new Error(`
+      Custom React components need to be in PascalCase.
+      You provided ${ComponentName}.
+
+      Please capitalize the first letter:
+      "${changeCase.upperCaseFirst(ComponentName)}"
+    `);
+  }
+
+  const componentDirectory = path.join(
+    __dirname,
+    '../../src/components',
+    ComponentName
+  );
+  createDirectory(componentDirectory);
+
+  const className = changeCase.camelCase(ComponentName);
+
+  // Create our index file, which points to our JS file
+  const indexPath = path.join(componentDirectory, 'index.js');
+  const indexTemplate = buildIndexFile(ComponentName);
+  fs.writeFileSync(indexPath, indexTemplate);
+
+  // Create and write JS to file
+  const componentPath = path.join(componentDirectory, `${ComponentName}.js`);
+  const componentTemplate = buildJSTemplate(ComponentName, className);
+  fs.writeFileSync(componentPath, componentTemplate);
+
+  // Create and write inline styles to file
+  const stylesPath = path.join(componentDirectory, `${ComponentName}.styles.js`);
+  const stylesTemplate = buildStylesTemplate(ComponentName, className);
+  fs.writeFileSync(stylesPath, stylesTemplate);
+
+  console.info(`Component ${ComponentName} successfully created!`);
+  return true;
+}
+
+
+// Helper Methods
+function createDirectory(componentDirectory) {
+  try {
+    fs.mkdirSync(componentDirectory);
+  } catch (err) {
+    console.error(err);
+    throw new Error(`Error creating directory for ${ComponentName}.`);
+  }
+
+  return componentDirectory;
+}
+
+function buildIndexFile(ComponentName) {
+  return `export { default } from './${ComponentName}';\n`
+}
+
+function buildJSTemplate(ComponentName, className) {
+  // Not digging the break in indentation here,
+  // but it's needed for the file to render correctly :(
+  return `\
+// @flow
+// eslint-disable-next-line no-unused-vars
+import React, { Component, PropTypes } from 'react';
+import { css } from '../../extensions/aphrodite';
+
+import styles from './${ComponentName}.styles';
+
+
+const ${ComponentName} = () => {
+  return (
+    <div className={css(styles.${className})}>
+      Your Component Here :)
+    </div>
+  );
+};
+
+${ComponentName}.propTypes = {
+
+};
+
+${ComponentName}.defaultProps = {
+
+};
+
+export default ${ComponentName};\n`;
+}
+
+function buildStylesTemplate(ComponentName, className) {
+  return `\
+import { StyleSheet } from 'aphrodite';
+
+export default StyleSheet.create({
+  ${className}: {
+
+  },
+});\n`;
+}
+
+const ComponentName = process.argv[2];
+run(ComponentName);
