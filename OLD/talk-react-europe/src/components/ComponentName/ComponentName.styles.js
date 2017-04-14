c38d79f815c10199a8923aa63f3359418e6d4fc3
Joshua Comeau <joshwcomeau@gmail.com>
2017-04-14 08:37:14 -0400

    Bring in old files

--- OLD/talk-react-europe/src/components/ComponentName/ComponentName.styles.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'aphrodite';
-
-export default StyleSheet.create({
-  componentName: {
-    fontFamily: 'monospace',
-    fontWeight: 300,
-  },
-});
--- OLD/talk-react-europe/src/slides/SpeakDemo/SpeakDemo.styles.js
+++
@@ -1,6 +0,0 @@
-import { StyleSheet } from 'aphrodite';
-
-export default StyleSheet.create({
-  speakDemo: {
-  },
-});
